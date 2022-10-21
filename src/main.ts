24e7e8753edd9904120916af64ebd333c857cd17
dieptv <diep.tv1999@gmail.com>
2022-10-21 10:33:22 +0700

    fix login

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://128.199.195.92'
+axios.defaults.baseURL = "http://128.199.195.92";
+axios.defaults.httpAgent = "http://128.199.195.92";
+axios.defaults.httpsAgent = "http://128.199.195.92";
+axios.defaults.proxy = false;
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.use(ElementPlus)
-app.mount('#app')
+app.use(ElementPlus);
+app.mount("#app");
